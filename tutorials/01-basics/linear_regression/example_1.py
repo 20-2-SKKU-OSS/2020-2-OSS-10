73d73aabd402726d62f5fb53908634b7fd379353
joqjoq966 <70578752+joqjoq966@users.noreply.github.com>
2020-12-01 22:47:26 +0900

    Create example_1.py

---
+++ tutorials/01-basics/linear_regression/example_1.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Hyper-parameters
+input_size = 1
+output_size = 1
+num_epochs = 60
+learning_rate = 0.001
+
+# Toy dataset
+x_train = np.array([[0.0],
+  [2.0],
+  [4.0],
+  [6.0],
+  [8.0],
+  [10.0],
+  [12.0],
+  [14.0],
+  [16.0],
+  [18.0],
+  [20.0],
+  [22.0],
+  [24.0],
+  [26.0],
+  [28.0],
+  [30.0],
+  [32.0],
+  [34.0],
+  [36.0],
+  [38.0]], dtype=np.float32)
+
+y_train = np.array([[0.0],
+  [4.0],
+  [8.0],
+  [12.0],
+  [16.0],
+  [20.0],
+  [24.0],
+  [28.0],
+  [32.0],
+  [36.0],
+  [40.0],
+  [44.0],
+  [48.0],
+  [52.0],
+  [56.0],
+  [60.0],
+  [64.0],
+  [68.0],
+  [72.0],
+  [76.0]], dtype=np.float32)
+
+# Linear regression model
+model = nn.Linear(input_size, output_size)
+
+# Loss and optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
+# Train the model
+for epoch in range(num_epochs):
+    # Convert numpy arrays to torch tensors
+    inputs = torch.from_numpy(x_train)
+    targets = torch.from_numpy(y_train)
+
+    # Forward pass
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+    
+    # Backward and optimize
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if (epoch+1) % 5 == 0:
+        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
+# Plot the graph
+predicted = model(torch.from_numpy(x_train)).detach().numpy()
+plt.plot(x_train, y_train, 'ro', label='Original data')
+plt.plot(x_train, predicted, label='Fitted line')
+plt.legend()
+plt.show()
+
+# Save the model checkpoint
+torch.save(model.state_dict(), 'model.ckpt')
