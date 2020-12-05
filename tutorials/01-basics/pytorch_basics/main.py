efbac1d1eb623028d917a3fab37aa2450393fa32
SongJae <33623118+SongJae@users.noreply.github.com>
2020-12-05 15:49:25 +0900

    Update main.py
    Add mul and matmul of tensor.

--- tutorials/01-basics/pytorch_basics/main.py
+++ tutorials/01-basics/pytorch_basics/main.py
@@ -215,6 +215,20 @@ m1 = torch.FloatTensor([[1, 2]])
 m2 = torch.FloatTensor([[3], [4]])
 print(m1 + m2)
 
+# Mul vs MatMul
+m1 = torch.FloatTensor([[1, 2], [3, 4]])
+m2 = torch.FloatTensor([[1], [2]])
+print('Shape of Matrix 1: ', m1.shape) # result 2 x 2
+print('Shape of Matrix 2: ', m2.shape) # result 2 x 1
+print(m1.matmul(m2)) # 2 x 1
+
+m1 = torch.FloatTensor([[1, 2], [3, 4]])
+m2 = torch.FloatTensor([[1], [2]])
+print('Shape of Matrix 1: ', m1.shape) # 2 x 2
+print('Shape of Matrix 2: ', m2.shape) # 2 x 1
+print(m1 * m2) # 2 x 2
+print(m1.mul(m2))
+
 # ================================================================== #
 #                      9. Basic numpy operations                     #
 # ================================================================== #
